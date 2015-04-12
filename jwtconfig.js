8c619ee5379b1a2d2492fef99341b16ee5ae5345
ckeyser <chriskeyser@hotmail.com>
2015-04-12 22:33:11 +0000

    final cleanup

--- jwtconfig.js
+++ jwtconfig.js
@@ -2,9 +2,8 @@ var express = require('express');
 
 //update with your app config information...
 var config = {
-    secret: new Buffer('1hV9....Zb2s', 'base64'),
-    audience: 'e5qA...rfClQ'
+        secret: new Buffer('1hV9lFfYn-7FQhkGWokKQE3Rt2o6bLj385T8ualKU9GkUktpsGv7Y4Ceo6yMZb2s', 'base64'),
+        audience: 'e5qATMYcwzE4uRqCapoO1NDkXZJrfClQ'
 };
-
 module.exports = config;
 
